b71141a04e0f40f542f8e42e017d970c24802f51
Ethan Vesely <Evesel259@wes-mec.org>
2021-03-23 17:59:04 -0700

    created homepage, moved css to scss, and added a darkmode

---
+++ script.js
@@ -0,0 +1,30 @@
+function dark () {
+    var element = document.body;
+    element.classList.toggle('darkmode');
+}
+
+var slideNumber = 1;
+showSlides(slideNumber);
+
+function changeSlide(n) {
+    showSlides(slideNumber += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideNumber = n);
+}
+
+function showSlides(n) {
+    var i = 0;
+    var slides = document.getElementsByClassName('overview');
+    if (n > slides.length) {
+        slideNumber = 1;
+    }
+    if (n < 1) {
+        slideNumber = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slides[slideNumber-1].style.display = 'block';
+}
